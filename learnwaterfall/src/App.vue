064b4c0eed6acd529fe6eb777ebc9675fb18eecf
Jim <wowdacom@gmail.com>
2020-03-09 19:17:23 +0800

    update waterfull

---
+++ learnwaterfall/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Waterfall/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import Waterfall from "./components/Waterfall.vue";
+
+export default {
+  name: 'app',
+  components: {
+    Waterfall
+  },
+  mounted () {
+
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  .my-grid {
+    // width: 100%;
+    // .item {
+    //   width: 30%;
+    //   border: solid 1px red;
+    // }
+  }
+}
+</style>
