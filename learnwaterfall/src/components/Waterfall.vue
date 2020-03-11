b3233977c52a2f2f1bd1ad44b4e3fb5c34ad09ec
Jim <wowdacom@gmail.com>
2020-03-11 18:42:46 +0800

    update

--- learnwaterfall/src/components/Waterfall.vue
+++ learnwaterfall/src/components/Waterfall.vue
@@ -1,117 +1,4 @@
 
-<style  lang="less" scoped>
-* {
-  margin: 0;
-}
-.container-water-fall {
-  // padding: 0 28px;
-  width: 100vw;
-  box-sizing: border-box;
-  h4 {
-    padding-top: 56px;
-    padding-bottom: 28px;
-    font-family: PingFangSC-Medium;
-    font-size: 36px;
-    color: #000000;
-    letter-spacing: 1px;
-    text-align: justify;
-  }
-  button {
-    background-color: #ff0;
-    color: #24292e;
-    border: 1px solid rgba(27, 31, 35, 0.2);
-    border-radius: 0.25em;
-    width: 100px;
-    line-height: 26px;
-    font-size: 13px;
-    margin: 4px 0;
-    margin-right: 4px;
-    cursor: pointer;
-    outline: none;
-    &.blue-light {
-      background: #27fbc2;
-    }
-  }
-  button:hover {
-    background-image: linear-gradient(-180deg, #fafbfc, #ccc 90%);
-  }
-
-  .cell-item {
-    width: 100%;
-    // margin-bottom: 18px;
-    background: #ffffff;
-    border: 2px solid #F0F0F0;
-    border-radius: 12px 12px 12px 12px;
-    overflow: hidden;
-    box-sizing: border-box;
-    img {
-      // border-radius: 12px 12px 0 0;
-      width: 100%;
-      height: auto;
-      display: block;
-    }
-    .item-body {
-      // border: 1px solid #F0F0F0;
-      padding: 12px;
-      .item-desc {
-        font-size: 15px;
-        color: #333333;
-        line-height: 15px;
-        font-weight: bold;
-      }
-      .item-footer {
-        margin-top: 22px;
-        position: relative;
-        display: flex;
-        align-items: center;
-        .avatar {
-          width: 44px;
-          height: 44px;
-          border-radius: 50%;
-          background-repeat: no-repeat;
-          background-size: contain;
-        }
-        .name {
-          max-width: 150px;
-          margin-left: 10px;
-          font-size: 14px;
-          color: #999999;
-        }
-        .like {
-          position: absolute;
-          right: 0;
-          display: flex;
-          align-items: center;
-          &.active {
-            i {
-            }
-            .like-total {
-              color: #FF4479;
-            }
-          }
-          i {
-            width: 28px;
-            display: block;
-          }
-          .like-total {
-            margin-left: 10px;
-            font-size: 12px;
-            color: #999999;
-          }
-        }
-      }
-    }
-  }
-}
-.githubdata {
-  float: right;
-  margin-right: 90px;
-  img {
-    width: 14px;
-    // height: 16px;
-  }
-}
-</style>
 <template>
   <div class="container-water-fall">
     <!-- <h1 style="position: fixed;left: 0;top: 100px;font-style: 15px;color:blue;z-index: 1000;">{{loadstatus}}</h1> -->
@@ -477,3 +364,117 @@ export default {
   }
 };
 </script>
+
+<style  lang="less" scoped>
+* {
+  margin: 0;
+}
+.container-water-fall {
+  // padding: 0 28px;
+  width: 100vw;
+  box-sizing: border-box;
+  h4 {
+    padding-top: 56px;
+    padding-bottom: 28px;
+    font-family: PingFangSC-Medium;
+    font-size: 36px;
+    color: #000000;
+    letter-spacing: 1px;
+    text-align: justify;
+  }
+  button {
+    background-color: #ff0;
+    color: #24292e;
+    border: 1px solid rgba(27, 31, 35, 0.2);
+    border-radius: 0.25em;
+    width: 100px;
+    line-height: 26px;
+    font-size: 13px;
+    margin: 4px 0;
+    margin-right: 4px;
+    cursor: pointer;
+    outline: none;
+    &.blue-light {
+      background: #27fbc2;
+    }
+  }
+  button:hover {
+    background-image: linear-gradient(-180deg, #fafbfc, #ccc 90%);
+  }
+
+  .cell-item {
+    width: 100%;
+    // margin-bottom: 18px;
+    background: #ffffff;
+    border: 2px solid #F0F0F0;
+    border-radius: 12px 12px 12px 12px;
+    overflow: hidden;
+    box-sizing: border-box;
+    img {
+      // border-radius: 12px 12px 0 0;
+      width: 100%;
+      height: auto;
+      display: block;
+    }
+    .item-body {
+      // border: 1px solid #F0F0F0;
+      padding: 12px;
+      .item-desc {
+        font-size: 15px;
+        color: #333333;
+        line-height: 15px;
+        font-weight: bold;
+      }
+      .item-footer {
+        margin-top: 22px;
+        position: relative;
+        display: flex;
+        align-items: center;
+        .avatar {
+          width: 44px;
+          height: 44px;
+          border-radius: 50%;
+          background-repeat: no-repeat;
+          background-size: contain;
+        }
+        .name {
+          max-width: 150px;
+          margin-left: 10px;
+          font-size: 14px;
+          color: #999999;
+        }
+        .like {
+          position: absolute;
+          right: 0;
+          display: flex;
+          align-items: center;
+          &.active {
+            i {
+            }
+            .like-total {
+              color: #FF4479;
+            }
+          }
+          i {
+            width: 28px;
+            display: block;
+          }
+          .like-total {
+            margin-left: 10px;
+            font-size: 12px;
+            color: #999999;
+          }
+        }
+      }
+    }
+  }
+}
+.githubdata {
+  float: right;
+  margin-right: 90px;
+  img {
+    width: 14px;
+    // height: 16px;
+  }
+}
+</style>
